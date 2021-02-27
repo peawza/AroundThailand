3539dded32e0e78f886c5b367a38858b21f606cc
peawkub <56231020+peawza@users.noreply.github.com>
2021-02-27 19:02:52 +0700

    update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:aroundthailand/main.dart';
+import 'package:projectflutterbeta/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
